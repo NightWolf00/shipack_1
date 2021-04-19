561bd589e9fa692037ed718d0270a7117559b922
NightWolf808 <65489492+NightWolf808@users.noreply.github.com>
2021-04-19 19:52:10 +0430

    Create test.php

---
+++ src/test.php
@@ -0,0 +1,13 @@
+<?php
+  namespace test;
+  class TestComp {
+    public function getRandomQuotes() {
+      $quotes = file_get_contents( __DIR__ . '/../inc/quotes.json' );
+      $quotes = json_decode( $quotes, true );
+      $index = mt_rand( 0, count( $quotes ) );
+      return $quotes[ $index ];
+    }
+    public function generate() {
+      return $this->getRandomQuotes();
+    }
+  }
